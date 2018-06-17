6a95071b8eb70716157c5e6d99386c4da54984bf
Dror Biran <drorb@wix.com>
2018-06-17 21:33:09 +0300

    Adding buttons to AddPost

--- src/posts/screens/AddPost.js
+++ src/posts/screens/AddPost.js
@@ -1,16 +1,38 @@
 import React, {PureComponent} from 'react';
 import {View, Text} from 'react-native-ui-lib';
+import {Navigation} from 'react-native-navigation';
+import PropTypes from 'prop-types';
 
 class AddPost extends PureComponent {
+
+  static propTypes = {
+    componentId: PropTypes.string
+  }
 
   static get options() {
     return {
       topBar: {
         title: {
           text: 'Add Post'
-        }
+        },
+        rightButtons: [{
+          id: 'saveBtn',
+          title: 'Save'
+        }],
+        leftButtons: [{
+          id: 'cancelBtn',
+          title: 'Cancel'
+        }]
       }
     };
+  }
+
+  onNavigationButtonPressed(buttonId) {
+    if (buttonId === 'cancelBtn') {
+      Navigation.dismissModal(this.props.componentId);
+    } else if (buttonId === 'saveBtn') {
+      alert('saveBtn');
+    }
   }
 
   render() {
